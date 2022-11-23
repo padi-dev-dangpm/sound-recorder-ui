77e2fe8aab8fa7eefc8e9907330b9fabeb51c0e7
Francisco Minh Dang <dangpm@paditech.com>
2022-11-23 23:14:12 +0700

    add all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReactMediaRecorder } from "react-media-recorder";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const { status, startRecording, stopRecording, mediaBlobUrl } =
+		useReactMediaRecorder({ video: false });
+	const handleSave = async () => {
+		const audioBlob = await fetch(mediaBlobUrl).then((r) => r.blob());
+		const audioFile = new File([audioBlob], "voice.wav", { type: "audio/wav" });
+
+		const response = await fetch(
+			"https://api-inference.huggingface.co/models/facebook/wav2vec2-base-960h",
+			{
+				headers: {
+					Authorization: "Bearer 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",
+				},
+				method: "POST",
+				body: audioFile,
+			}
+		);
+		const result = await response.json();
+		console.log(result);
+	};
+	return (
+		<div>
+			<p>{status}</p>
+			<button onClick={startRecording}>Start Recording</button>
+			<button onClick={stopRecording}>Stop Recording</button>
+			<button onClick={handleSave}>Save</button>
+
+			<video src={mediaBlobUrl} controls autoPlay loop />
+		</div>
+	);
+};
 
 export default App;
